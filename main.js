3e040150ff73d24e2b2d2cfaded68633a9f4cd2f
Adam Buksowicz <96523965+foxxo-dev@users.noreply.github.com>
2024-03-10 11:47:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -20,9 +20,10 @@ const body = document.querySelector('html');
 html[data-theme='light'] {
   filter: invert(1) hue-rotate(180deg);
 }
-html[data-theme='light] svg,
-html[data-theme='light] img,
-html[data-theme='light] canvas {
+html[data-theme='light'] svg,
+html[data-theme='light'] img,
+html[data-theme='light'] canvas,
+html[data-theme='light'] video{
   filter: invert(1) hue-rotate(180deg);
 }
 `;
